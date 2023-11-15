8cfa9c9ff518f8e5e32ec9bd40fb7a37b6543363
Breezevna <breezevna@Breezevnas-PC.home>
2023-11-15 16:41:40 +0100

    All files added

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# Codsoft_Task1_NumberGuessing
+# Number Guessing Game
+
+## Overview
+
+The Number Guessing Game is a simple console-based game where the player tries to guess a randomly generated number within a specified range. The game provides an entertaining and interactive way to test your guessing skills.
+
+## Features
+
+- **Random Number Generation:** The game generates a random number within a specified range for the player to guess.
+
+- **User Interaction:** Players can input their guesses through the console, making the game interactive and engaging.
+
+- **Flexible Range:** Players can choose whether to stick with the default range of 1 to 100 or customize it by providing minimum and maximum values.
+
+- **Limited Attempts:** To add challenge, players are given a limited number of attempts to guess the correct number.
+
+- **Graceful Input Handling:** The game handles invalid inputs gracefully, providing a smooth user experience.
+
+## How to Play
+
+1. **Start the Game:**
+   - Run the program and enter your name when prompted.
+   - Choose whether to use the default range (1 to 100) or customize it.
+
+2. **Guess the Number:**
+   - Input your guess when prompted.
+   - The game will provide feedback if your guess is too high or too low.
+
+3. **Winning:**
+   - If you guess the correct number within the given attempts, you win!
+   - The game will display the number of attempts it took to guess correctly.
+
+4. **Game Stopping:**
+   - You can stop the game at any time by entering "stop" when prompted for a guess.
+
+## Building and Running
+
+- Ensure you have a C++ compiler installed.
+- Compile the game using the provided source code.
+- Run the compiled executable to start the game.
+
+## Customization
+
+Feel free to customize the game further by modifying the source code. You can adjust the range, the maximum number of attempts, or add additional features to enhance the gaming experience.
+
+## Acknowledgments
+
+This Number Guessing Game project is a simple yet fun implementation that can be used as a starting point for more complex console-based games. Enjoy guessing, and have fun exploring the world of C++ game development!
---
+++ game.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <limits>
+
+class NumberGuessingGame
+{
+	private:
+	int hiddenNum;
+	int attempts;
+	int maxAttempts;
+	std::string playerName;
+	public:
+	int generateNum(int min, int max)
+	{
+		return (std::rand() % (max - min + 1) + min);
+	}
+
+	NumberGuessingGame(std::string name, int min, int max) : playerName(name), attempts(0)
+	{
+		std::srand(std::time(0));
+		hiddenNum = generateNum(min, max);
+		maxAttempts = static_cast<int>((max - min) * 1.5);
+	}
+	NumberGuessingGame(std::string name, int maxAttempts) : playerName(name), attempts(0), maxAttempts(maxAttempts)
+	{
+		std::srand(std::time(0));
+		hiddenNum = generateNum(1, 100);
+	}
+
+	void play()
+	{
+		std::cout << "Welcome, " << playerName << ", to the Number Guessing Game\n";
+		std::cout << "You have " << maxAttempts << " attempts to guess the number\n";
+		int guess;
+		do
+		{
+			std::string userInput;
+			std::cout << "Enter your guess (or 'stop' to end the game): ";
+			std::cin >> userInput;
+
+			if (userInput == "stop")
+			{
+				std::cout << "Game stopped by the player.\n";
+				return;
+			}
+			try
+			{
+				guess = std::stoi(userInput);
+			}
+			catch (const std::invalid_argument&)
+			{
+				std::cerr << "Invalid input. Please enter a valid number or 'stop'.\n";
+				std::cin.clear();
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				continue;
+			}
+			attempts++;
+			if (guess == hiddenNum)
+				std::cout << "Congratulations, " << playerName << "! You guessed the correct number in " << attempts << " attempts.\n";
+			else if (guess < hiddenNum)
+				std::cout << "Too low! Try again.\n";
+			else
+				std::cout << "Too high! Try again.\n";
+		} while (attempts < maxAttempts && guess != hiddenNum);
+		if (attempts >= maxAttempts) {
+            std::cout << "Sorry, " << playerName << ", you've reached the maximum number of attempts. The correct number was: " << hiddenNum << "\n";
+        }
+	}
+};
+int main()
+{
+	std::string playerName;
+	int min, max;
+	std::string chose = "";
+
+	std::cout << "Welcome to he Number Guessing game!\n";
+	std::cout << "The rules are simple! Just guess the number!\n";
+
+	std::cout << "Enter your name: ";
+	std::getline(std::cin, playerName);
+
+	std::cout << "The game generates random number between 1 and 100. Do you want to change the range?\n Yes - 1, No - 0, Stop game - stop \n ";
+	std::getline(std::cin, chose);
+
+	while (chose != "1" && chose != "0" && chose != "stop")
+	{
+		std::string userInput;
+		std::cerr << "Enter 1 or 2  ('stop' to end the game): ";
+		std::cin >> userInput;
+		if (userInput == "stop")
+			{
+				std::cout << "Game stopped by the player.\n";
+				return (0);
+			}
+			try
+			{
+				chose = std::stoi(userInput);
+			}
+			catch (const std::invalid_argument&)
+			{
+				std::cout << "Invalid input. Please enter a valid number or 'stop'.\n";
+				std::cin.clear();
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				continue;
+			}
+	}
+
+	if (chose == "1")
+	{
+		std::cout << "Enter the minimum value for the range: ";
+		std::cin >> min;
+
+		std::cout << "Enter the maximum value for the range: ";
+		std::cin >> max;
+
+		while (min >= max)
+		{
+			std::cerr << "Invalid range. Minimum value is greater than maximum value.\n";
+			std::cout << "Enter the minimum value for the range: ";
+			std::cin >> min;
+			std::cout << "Enter the maximum value for the range: ";
+			std::cin >> max;
+		}
+		NumberGuessingGame game(playerName, min, max);
+		game.play();
+	}
+	else if (chose == "0")
+	{
+		int maxAttempts;
+		std::cout << "Enter the maximum number of attempts: ";
+		std::cin >> maxAttempts;
+
+		NumberGuessingGame game(playerName, maxAttempts);
+		game.play();
+	}
+	return (0);
+}
